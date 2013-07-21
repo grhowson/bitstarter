ca8aae4098b11025cec961790ad97b122a36133a
grhowson <grhowson1@gmail.com>
2013-07-21 11:46:56 +0000

    updated web.js

--- web.js
+++ web.js
@@ -2,8 +2,13 @@ var express = require('express');
 var app = express();
 app.use(express.logger());
 
+var fs = require('fs');
+var fbuf = fs.readFileSync('index.html');
+var fstr = fbuf.toString();
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(fstr);
 });
 
 var port = process.env.PORT || 5000;
